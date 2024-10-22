02a6c8905cbeddd30d3f5976e2df97ec07e1afa6
lenscorpas <anupam.sharma@lenscorp.ai>
2024-10-22 12:08:39 +0530

    portfolio

---
+++ src/components/Home/Type.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Software Engineer",
+          "React Native Developer",
+          "React Js Developer",
+          "Open Source Contributor",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
